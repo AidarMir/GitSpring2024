cc99d158e625a1060ac2bb1bc5892e62f163e38a
Aidar <AidarMirlanov28@gmail.com>
2024-08-08 19:27:14 +0900

    Aidarrr

---
+++ src/main/java/sports/Aidarrr.java
@@ -0,0 +1,7 @@
+package sports;
+
+public class Aidarrr {
+    public static void main(String[] args) {
+        System.out.println("Aidarr");
+    }
+}
--- src/main/java/sports/football.java
+++ src/main/java/sports/football.java
@@ -2,6 +2,6 @@ package sports;
 
 public class football {
     public static void main(String[] args) {
-        System.out.println("Football");
+        System.out.println("Footballпше ");
     }
 }
