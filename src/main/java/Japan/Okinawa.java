5a29719fb18e04fedf486578ea63189ce46c1958
Aidar <AidarMirlanov28@gmail.com>
2024-08-12 01:38:19 +0900

    JP

---
+++ src/main/java/Japan/Okinawa.java
@@ -0,0 +1,7 @@
+package Japan;
+
+public class Okinawa {
+    public static void main(String[] args) {
+        System.out.println("Naha");
+    }
+}
---
+++ src/main/java/Japan/Tokyo.java
@@ -0,0 +1,7 @@
+package Japan;
+
+public class Tokyo {
+    public static void main(String[] args) {
+        System.out.println("Tokyo city");
+    }
+}
