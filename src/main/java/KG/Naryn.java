b6e775f5dc74430054b4c25ffc3a275077a6b980
Aidar <AidarMirlanov28@gmail.com>
2024-08-12 01:44:44 +0900

    kg2

---
+++ src/main/java/KG/Naryn.java
@@ -0,0 +1,7 @@
+package KG;
+
+public class Naryn {
+    public static void main(String[] args) {
+        System.out.println("Naryn");
+    }
+}
---
+++ src/main/java/KG/bishkek.java
@@ -0,0 +1,7 @@
+package KG;
+
+public class bishkek {
+    public static void main(String[] args) {
+        System.out.println("Bishkek");
+    }
+}
