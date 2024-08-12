fb620680878e60d03796be094c6dfd92012a395b
Aidar <AidarMirlanov28@gmail.com>
2024-08-12 20:36:05 +0900

    codingbat/extraEnd

---
+++ src/main/java/String1/extraEnd.java
@@ -0,0 +1,8 @@
+package String1;
+
+public class extraEnd {
+    public String extraEnd(String str) {
+        return str.substring(str.length()-2)+str.substring(str.length()-2)+str.substring(str.length()-2);
+
+    }
+}
