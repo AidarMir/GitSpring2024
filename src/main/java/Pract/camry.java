b03714537fe34c8eb8a2a79bdef16d5901dabcbd
Aidar <AidarMirlanov28@gmail.com>
2024-08-14 23:25:37 +0900

    pro75

--- src/main/java/Pract/camry.java
+++ src/main/java/Pract/camry.java
@@ -3,6 +3,7 @@ package Pract;
 public class camry {
     public static void main(String[] args) {
         System.out.println("camry");
-        System.out.println("camry75");
+        System.out.println("camry75")
+        ;
     }
 }
